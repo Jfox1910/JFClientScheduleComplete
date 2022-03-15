e77000b0aa84f5f14a722cac643a605fb1f8d9c7
Jfox1910 <37463263+Jfox1910@users.noreply.github.com>
2022-03-15 17:34:42 -0400

    Current

--- src/Controller/ModCustomerController.java
+++ src/Controller/ModCustomerController.java
@@ -1,18 +1,23 @@
 package Controller;
 
+import Dao.DaoCustomers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Optional;
+import java.util.ResourceBundle;
 
-public class ModCustomerController {
+public class ModCustomerController implements Initializable {
 
     private Stage stage;
     private Scene scene;
@@ -32,5 +37,23 @@ public class ModCustomerController {
             stage.setScene(scene);
             stage.show();
         }
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //Customers = DaoCustomers.getAllCustomers();
+        //customersTableView.setItems(customers);
+        //customerIdCol.setCellValueFactory(new PropertyValueFactory<>("customerID"));
+        //customerNameCol.setCellValueFactory(new PropertyValueFactory<>("customerName"));
+        //customerAddyCol.setCellValueFactory(new PropertyValueFactory<>("customerAddress"));
+        //customerZipCol.setCellValueFactory(new PropertyValueFactory<>("customerZip"));
+        //customerPhoneCol.setCellValueFactory(new PropertyValueFactory<>("customerPhone"));
+
+        /**
+         * EDIT TO SHOW ACTUAL DIVISION INSTEAD OF THE ID*/
+
+        //customerDivisionCol.setCellValueFactory(new PropertyValueFactory<>("customerDivision"));
+        //customerDivisionCol.setCellValueFactory(new PropertyValueFactory<>("customerCountry"));
     }
 }
